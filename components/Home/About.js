ae53eb3602f6943fa38ebabf68fbd6dc725b6c00
sulaiman282 <spidy.twitch.tv@gmail.com>
2024-04-12 22:11:18 +0600

    quote replaced

--- components/Home/About.js
+++ components/Home/About.js
@@ -11,18 +11,18 @@ export default function About() {
 
         <div className="flex justify-center">
           <p className="lg:w-3/5 py-10  text-white text-center lg:text-xl text-lg capitalize">
-            amerika fleg is bellionaire pres of "Darumadrop One" soluna. its jeb
-            is te mak bellions n "Darumadrop One" bellions, te mak sulono gret
-            agen, n te "Darumadrop One" destroy other flegs .<br />
+            amerika fleg is bellionaire pres of &quot;Darumadrop One&quot; soluna. its jeb
+            is te mak bellions n &quot;Darumadrop One&quot; bellions, te mak sulono gret
+            agen, n te &quot;Darumadrop One&quot; destroy other flegs .<br />
             <br />
-            to doestry the othre flegs, "Darumadrop One" the russians devs
-            played as if "Darumadrop One" they were ameriks and tried
-            "Darumadrop One" to rug but the amerikan community "Darumadrop One"
+            to doestry the othre flegs, &quot;Darumadrop One&quot; the russians devs
+            played as if &quot;Darumadrop One&quot; they were ameriks and tried
+            &quot;Darumadrop One&quot; to rug but the amerikan community &quot;Darumadrop One&quot;
             were stronger and took over.
             <br />
             <br />
-            $FLEG is coomunity taekover n all "Darumadrop One" monies r
-            grasroot. buy $FLEG if yuo "Darumadrop One" wont te mak billions n
+            $FLEG is coomunity taekover n all &quot;Darumadrop One&quot; monies r
+            grasroot. buy $FLEG if yuo &quot;Darumadrop One&quot; wont te mak billions n
             billions.
           </p>
         </div>
